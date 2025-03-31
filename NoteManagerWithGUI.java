1453ee7c49cf531465a805abfc43adaa29137ea7
Prabinthapadev21 <codinghub57@gmail.com>
2025-03-31 20:58:48 +0545

    Updated

--- NoteManagerWithGUI.java
+++ NoteManagerWithGUI.java
@@ -3,5 +3,10 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.*;
-public class NoteManagerWithGUI {
-}
+public class NoteManagerWithGUI extends JFrame{
+    private JTextArea textArea;
+    private JButton saveButton, readButton, appendButton, deleteButton, chooseFileButton, darkModeButton;
+    private File selectedFile = new File(System.getProperty("user.home") + "/Desktop/notes.txt"); // Default file
+    private boolean isDarkMode = false;
+
+
